ead2feead44be0da5843224ae2609e377a8affac
chaitanya-hase <hasechaitanya@gmail.com>
2026-06-28 19:49:19 +0530

    Convert jdbcTrial from submodule to normal folder

---
+++ jdbcTrial/JDBCLaunch.java
@@ -0,0 +1,34 @@
+package com.jdbcTrial;
+import java.sql.*;
+public class JDBCLaunch {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		//load and register driver
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		//establish connection
+		String url="jdbc:mysql://localhost:3306/testdb";
+		String user="root";
+		String password="root123";
+		Connection con=DriverManager.getConnection(url,user,password);
+		
+		//create statement
+		Statement stm=con.createStatement();
+		
+		int count=stm.executeUpdate("insert into StudentInfo(id,sname,salary) values(4,'sita',10000000000)");
+		
+		//result processing
+		System.out.println(count + " row inserted.");
+		ResultSet rs=stm.executeQuery("Select * from Studentinfo");
+		
+		while(rs.next()) {
+			System.out.println(rs.getInt("id")+" "+rs.getString("sname")+" "+rs.getDouble("salary"));
+		}
+		//close
+		rs.close();
+		stm.close();
+		con.close();
+
+	}
+
+}
